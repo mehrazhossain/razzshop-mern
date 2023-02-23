ac05a186f3221e2cacddc508f2979e2531773c69
Mehraz Hossain <mehrazhossain54.com>
2023-02-23 16:39:53 +0600

    replace proxy address

---
+++ frontend/build/precache-manifest.a63375d7eff675857a0c5aa9900fbb03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b85aef493fd11fa0b4d34ce95bcac2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c2569c21b7ed5275a8f",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2569c21b7ed5275a8f",
+    "url": "/static/js/main.eb57be60.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
